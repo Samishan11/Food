b2bd241076f0982189ae4bb90f9ec170777db857
Samishan <samishanthapa0@gmail.com>
2022-07-27 09:34:54 +0545

    change

--- src/components/Context/FoodContext.js
+++ src/components/Context/FoodContext.js
@@ -5,7 +5,7 @@ export const Foodprovider = (props) => {
     const [contextfood, setContextFood] = useState([]);
     useEffect(() => {
         const getfood = () => {
-            axios.get("/getallfood").then(data => {
+            axios.get("api/getallfood").then(data => {
                 setContextFood(data.data)
             }).catch(e => {
                 console.log(e);
